432017e3fcbac668a274088f0b8afa987b636fcc
Steven Steinberg <sstein18@gmail.com>
2015-05-02 21:05:11 -0400

    Commiting Contact List Exercise

---
+++ Lesson9B/src/app.js
@@ -0,0 +1,82 @@
+var myApp = angular.module('contactList', ['ui.router']);
+
+myApp.directive('contactItem', function(){
+	return {
+		// template: '<section><h3>Contact Item</h3></section>',
+		templateUrl: './contact-item.html', //why doesn't this work?
+		controller: 'ContactController',
+		controllerAs: 'c',
+		scope: {
+			contact: "="
+		},
+		
+		bindToController: true
+	} 
+});
+
+myApp.config( function( $stateProvider, $urlRouterProvider ) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/add-contact");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('add-contact', {
+      url: "/add-contact",
+      templateUrl: "partials/add-contact.html"
+    })
+    .state('add-contact.list', {
+      url: "/list",
+      templateUrl: "partials/add-contact.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('show-contact', {
+      url: "/show-contact",
+      templateUrl: "partials/show-contact.html"
+    })
+    .state('show-contact.list', {
+      url: "/show-contact.list",
+      templateUrl: "partials/show-contact.list.html",
+      controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+      }
+    });
+});
+
+myApp.controller('ContactController', function(){
+	var self = this;
+});
+
+myApp.controller('FullController', function(){
+	var self = this;
+
+
+	self.contacts = [];
+
+	self.newContact = {
+		firstName: "",
+		lastName: "",
+		description: "",
+		telephoneNumber: "",
+		email: ""
+	}
+
+	self.addContact = function(){
+		self.contacts.push( self.newContact );
+
+		console.log( self.contacts );
+
+		self.newContact = {
+			firstName: "",
+			lastName: "",
+			description: "",
+			telephoneNumber: "",
+			email: ""
+		}
+	};
+
+});
+
+
---
+++ Lesson9B/src/app/index.js
@@ -0,0 +1,93 @@
+var myApp = angular.module('contactList', ['ui.router']);
+
+myApp.config( function( $stateProvider, $urlRouterProvider ) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/add-contact");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('add-contact', {
+      url: "/add-contact",
+      templateUrl: "assets/partials/add-contact.html",
+      controller: 'ContactController'
+    })
+    .state('show-contact', {
+      url: "/show-contact",
+      templateUrl: "assets/partials/show-contact.html",
+        controller: "ShowController" 
+    });
+});
+
+myApp.factory('ContactDTO', function() {
+  function ContactDTO() {
+  }
+  return ContactDTO;
+ });
+
+myApp.service("ContactService",function() {
+
+  this.contactsArray = [];
+
+  this.addContact = function (person) {
+    
+    this.contactsArray.push(person);
+
+    console.log(this.contactsArray);
+  }
+
+});
+
+
+myApp.controller('ContactController', function($scope,ContactDTO,ContactService) {
+
+  $scope.person = new ContactDTO();
+
+  $scope.addContact = function() {
+    
+    ContactService.addContact($scope.person);
+
+    $scope.person = new ContactDTO();
+
+  }
+
+});
+
+myApp.controller('ShowController', function($scope,ContactService) {
+
+ $scope.contacts = ContactService.contactsArray;
+
+});
+
+
+// myApp.controller('FullController', function(){
+// 	var self = this;
+
+
+// 	self.contacts = [];
+
+// 	self.newContact = {
+// 		firstName: "",
+// 		lastName: "",
+// 		description: "",
+// 		telephoneNumber: "",
+// 		email: ""
+// 	}
+
+// 	self.addContact = function(){
+// 		self.contacts.push( self.newContact );
+
+// 		console.log( self.contacts );
+
+// 		self.newContact = {
+// 			firstName: "",
+// 			lastName: "",
+// 			description: "",
+// 			telephoneNumber: "",
+// 			email: ""
+// 		}
+// 	};
+
+// });
+
+
